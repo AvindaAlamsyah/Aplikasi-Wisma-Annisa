a61052e756e75861f5ef6d38ea1672ff6ccb8743
alfariscahya7 <alfariscahya7@gmail.com>
2018-05-15 11:22:13 +0700

    terbaruu belum register [new] . ,masih otw .

---
+++ wismaannisa3/app/src/main/java/e/hmmmcahya/wismaannisa/daftar.java
@@ -0,0 +1,13 @@
+package e.hmmmcahya.wismaannisa;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class daftar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_daftar);
+    }
+}
--- wismaannisa3/app/src/main/java/e/hmmmcahya/wismaannisa/opsi.java
+++ wismaannisa3/app/src/main/java/e/hmmmcahya/wismaannisa/opsi.java
@@ -1,13 +1,35 @@
 package e.hmmmcahya.wismaannisa;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 
 public class opsi extends AppCompatActivity {
-
+Button login1;
+Button daftar1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_opsi);
+
+        login1 = (Button)findViewById(R.id.btnlgn);
+        login1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(),login.class);
+                view.getContext().startActivity(intent);}
+        });
+        daftar1 = (Button)findViewById(R.id.btndftr);
+        daftar1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent1 = new Intent(view.getContext(),daftar.class);
+                view.getContext().startActivity(intent1);}
+            });
+
+
     }
 }
